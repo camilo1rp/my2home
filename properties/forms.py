e6f4d136086ac4f4c4880fe1502f95a3bbb84000
Camilo Romero <camilo.romero@tncolombia.com.co>
2019-12-23 17:09:48 -0500

    added labels and placeholders for address form

--- properties/forms.py
+++ properties/forms.py
@@ -9,7 +9,6 @@ class PropertyForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['description'].widget.attrs.update(rows='2', cols='40')
         self.fields['owner'].empty_label = _('select owner')
-        self.fields['manager'].empty_label = _('select manager')
 
 
     class Meta:
@@ -18,10 +17,24 @@ class PropertyForm(forms.ModelForm):
 
 
 class AddressColForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['tipo_via'].widget.attrs.update(placeholder='ej. Calle')
+        self.fields['via'].widget.attrs.update(placeholder='ej. 10')
+        self.fields['prefijo_via'].widget.attrs.update(placeholder='ej. A')
+        self.fields['numero'].widget.attrs.update(placeholder='ej. 11')
+        self.fields['prefijo_numero'].widget.attrs.update(placeholder='ej. BIS')
+        self.fields['placa'].widget.attrs.update(placeholder='ej. 12')
+        self.fields['ciudad'].widget.attrs.update(placeholder='ej. Bogota')
+        self.fields['departamento'].widget.attrs.update(placeholder='ej. Cundinamarca')
+        self.fields['barrio'].widget.attrs.update(placeholder='ej. Cedritos')
+        self.fields['prefijo_via'].label = 'prefijo'
+        self.fields['prefijo_numero'].label = 'prefijo'
+
+
     class Meta:
         model = AddressCol
         exclude = []
-
 
 class ImageForm(forms.Form):
     property_id = forms.CharField(max_length=12)
