6266110991b12065df5e3dfb367af265c1c0db98
fernando737 <fernando.perezdp1@gmail.com>
2020-01-12 20:02:57 +0000

    Fix templates delete %load staticfiles%

--- myhome/settings_development.py
+++ myhome/settings_development.py
@@ -147,7 +147,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
+#https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
