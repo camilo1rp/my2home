d5f13602fff7dce3a2d92d1153f2f88263aec6f9
Camilo Romero <camilo.romero@tncolombia.com.co>
2020-01-13 11:09:44 -0500

    fixed login url and view. simple template, added username validation: check if user already exists

--- profiles/forms.py
+++ profiles/forms.py
@@ -21,3 +21,10 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('phone', 'gender', 'city', 'company')
+
+    def clean_username(self):
+        user = self.cleaned_data['username']
+        usernames = [users.user.username for users in Profile.objects.all()]
+        if user in usernames:
+            self.add_error('username', "Username is already taken")
+        return user
