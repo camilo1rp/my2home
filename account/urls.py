7cf7c12f9563e21ff09a52c79a785b1810760d74
jhon Quitian <datolo326@gmail.com>
2020-01-21 15:24:13 -0500

    property_Card_scroll - view.py - url - Dashboard creacion de dasboard con style de carrusel

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    path('property_card/', views.card, name='card'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from properties.models import Property
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', )
+	return render(request, 'account/dashboard.html',)
+
+def card(request):
+	latest_properties = Property.objects.order_by('-created')
+	return render(request, 'properties/property_card_scroll.html', {'properties': latest_properties})
 
 
