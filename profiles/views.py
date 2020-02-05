1c0c40a3927b739a54848d38162501834fe69bf6
Camilo Romero <camilo.romero@tncolombia.com.co>
2020-02-05 16:26:32 -0500

    add: account login and sing up: add profile template into sign up and added error messages change: account login and sing up: change styles change: account login and sing up: flow of forms and redirects

--- profiles/views.py
+++ profiles/views.py
@@ -10,7 +10,6 @@ from .models import Profile
 class CreateUser(CreateView):
     form_class = UserForm
     template_name = "profiles/create.html"
-    success_url = reverse_lazy('account:login')
 
 
 class ProfileUpdate(UpdateView):
@@ -19,6 +18,13 @@ class ProfileUpdate(UpdateView):
     template_name = "profiles/profile.html"
     pk_url_kwarg = 'user_id'
     context_object_name = 'profile'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in the profile id
+        context['pro_id'] = self.kwargs['user_id']
+        return context
 
 
 
