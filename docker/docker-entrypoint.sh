f2501f2a83b99bd3d247118db06b8fb0d93e8737
fernando737 <fernando.perezdp1@gmail.com>
2020-02-11 19:05:55 +0000

    fix docker composer and entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -11,6 +11,26 @@ while ! python3 /opt/my2home/manage.py migrate 2>&1; do
 	sleep 5
 done
 
+# create superuser
+create_superuser="
+import django
+django.setup()
+from django.contrib.auth.models import User
+try:
+    User.objects.create_superuser('$DJANGO_SUPERUSER_NAME', '$DJANGO_SUPERUSER_MAIL', '$DJANGO_SUPERUSER_PASS')
+except Exception:
+    pass
+"
+create_superuser() {
+    if [ -z "$DJANGO_SUPERUSER_NAME" ] || [ -z "$DJANGO_SUPERUSER_MAIL" ] || [ -z "$DJANGO_SUPERUSER_PASS" ]; then
+        echo "Environment variables for database not set, not creating superuser."
+    else
+        echo "Creating superuser"
+        python3 -c "$create_superuser"
+    fi
+}
+
+
 # run python3 manage.py collectstatic
 python3 /opt/my2home/manage.py collectstatic --noinput
 
