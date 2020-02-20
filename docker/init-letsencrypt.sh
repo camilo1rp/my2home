9a2f6e3e1b4c8015965b86c700b49b8fdf356e8f
fernando737 <fernando.perezdp1@gmail.com>
2020-02-20 12:11:43 +0000

    Fix initletsencrypt and nginx.conf

--- docker/init-letsencrypt.sh
+++ docker/init-letsencrypt.sh
@@ -11,18 +11,18 @@ data_path="./data/certbot"
 email="info@inmueblesfusa.com" #Adding a valid address is strongly recommended
 staging=0 #Set to 1 if you are testing your setup to avoid hitting request limit
 
-if [-d "$data_path" ]; then
+if [ -d "$data_path" ]; then
 	read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-	if [ "$decision" != "Y" ] && ["$decision" != "y" ]; then
+	if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
 		exit
 	fi
 fi
 
-if [! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
 	echo "### Downloading recommended TLS parameters ..."
 	mkdir -p "$data_path/conf"
 	curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-	curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem >"$data_path/conf/ssl-dhparams.pem"
+	curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
 	echo
 fi
 
